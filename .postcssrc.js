9face52f53cd2a3846274e37ee93b908cf127799
hxxeox <df21234@naver.com>
2022-12-08 16:15:44 +0900

    Create project

---
+++ .postcssrc.js
@@ -0,0 +1,17 @@
+// ESM (자바스크립트 모듈의 약어)
+// CommonJs
+
+
+//import autoprefixer from 'autoprefixer'
+
+
+//export {
+//    plugin: {
+//        autoprefixer
+//    }
+// }
+module.exports = {
+    plugins:[
+        require('autoprefixer')
+    ]
+}
---
+++ js/main.js
@@ -0,0 +1,8 @@
+console.log('Hello Parcel!')
+// async는 비동기 함수
+async function test() {
+    const promise = Promise.resolve(123)
+    console.log(await promise)
+}
+
+test()
